163fd5fa85f0bbda1914aa85ae655a844cb49722
Ivan Pavic <ivan96jp@gmail.com>
2023-07-19 09:36:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='min-h-screen flex items-center justify-center lg:mx-48 text-white font-poppins'>
+      <h1>Create lovely animations with Lottie.</h1>
+    </div>
+  )
+}
+
+export default App
